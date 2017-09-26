c6cd621adac2586af55ad433f54a26ce694e33a5
pputwee <32306299+pputwee@users.noreply.github.com>
2017-09-27 01:01:10 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -12,6 +12,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_close($ch);
 
 echo $result;
