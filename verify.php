ba7e4b9f44dd294b912ef7da9a60fd48311eef89
pputwee <32306299+pputwee@users.noreply.github.com>
2017-09-27 01:03:10 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,19 +1,17 @@
 <?php
 $access_token = 'yhTnvirBNNgU9cWjVEGouJkW1ld6uTWtQRbmyEHU6x4RbFp/0sFhSdtVutwIKdXXv7ugmPm1JucjDrAjfgPAZ6pB9CNhBucUYHmEyVolW1/HbLB6czWwC1a6oHgfvOIldzFiSnrZ/Tg1VwNGqHUW+gdB04t89/1O/w1cDnyilFU=';
 
-$proxy = 'proxyurl:port';
-$proxyauth = 'username:password';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_close($ch);
 
 echo $result;
